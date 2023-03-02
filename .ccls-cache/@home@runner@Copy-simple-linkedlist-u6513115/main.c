bbda9bfcb25446072ca8eb625db4ffc02e533bbd
u6513115 <122536267+u6513115@users.noreply.github.com>
2023-03-02 01:27:59 +0000

    Exercise V

--- .ccls-cache/@home@runner@Copy-simple-linkedlist-u6513115/main.c
+++ .ccls-cache/@home@runner@Copy-simple-linkedlist-u6513115/main.c
@@ -5,73 +5,42 @@
 //  Created by Mingmanas Sivaraksa on 4/2/2566 BE.
 //
 
+/*  Exercise V Use malloc to create all nodes, instead of create a struct!!
+        //use a loop to help
+
+    */
+
 #include "node.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, const char *argv[]) {
   int c = 5;
-  struct node a, b, *head;
-  a.value = c;
-  a.next = &b;
-  head = &a;
-  b.value = head->value + 3;
+  struct node b, *head;
+  typedef struct node Node;
+  typedef Node *NodePtr;
 
+  NodePtr p = (NodePtr)malloc(sizeof(Node));
+  head = p;
+  p->value = c;
 
-  /*  Exercise I
-      1. Add 1 more than at the end
-      2. Add value(11)
-      3. Make next become NULL
-   */
+  int i, n = 4;
+  for (i = 0; i < n; i++) {
+    p->next = (NodePtr)malloc(sizeof(Node));
+    p = p->next;
+    p->value = c + i + 1;
+    p->next = NULL; //จะได้ชัวร์ว่ามี next
+  }
 
-    struct node d;
-    b.next=&d;
-    d.value = 11;
-    d.next = NULL;
-
-  /*  Exercise II
-          1. Add 1 more than at the begining!!!!
-          2. Add value (2)
-
-  */
-  
-    struct node z;
-  z.value = 2;
-  z.next = &a;
-  head = &z;
-
-  
   typedef struct node *NodePtr;
   NodePtr tmp = head; // add temp value to faciliate
 
-
-  /*  Exercise IV change to while loop!! (you can use NULL to help)
-
-        while(){
-           printf("%3d", tmp->value);
-          // What is missing???
-       }
-   */
-  printf("Exercise IV\n");
-    tmp = head;
   while (tmp != NULL) {
     printf("%3d->", tmp->value);
     tmp = tmp->next;
 
     // What is missing???
   }
-  printf("NULL\n");
-  printf("-----------------------\n");
-
-  /*  Exercise V Use malloc to create all nodes, instead of create a struct!!
-          //use a loop to help
-
-      */
-
-  /*  Exercise VI Free all node !!
-       //use a loop to help
-
-   */
 
   return 0;
 }
